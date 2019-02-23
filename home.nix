a8c82f10470a41fae886e7e3e5d1aaa6cad6c0ca
cvoges12 <vogesclayton@gmail.com>
2019-02-23 11:25:54 -0600

    first commit

---
+++ home.nix
@@ -0,0 +1,77 @@
+{ config, pkgs, ... }:
+
+# https://rycee.gitlab.io/home-manager/options.html
+
+let
+  name = "Clayton Voges";
+  gmail = "vogesclayton@gmail.com";
+  githubUsername = "cvoges12";
+  dots = "/home/cvoges12/dots";
+  scripts = "/home/cvoges12/scripts";
+  maildir = "/home/cvoges12/Mail";
+
+in
+
+{
+  #accounts.email = {
+  #  maildirBasePath = "${maildir}";
+  #  accounts = {
+  #    gmail = {
+  #      address = "${gmail}";
+  #      userName = "${gmail}";
+  #      flavor = "gmail.com";
+  #      #passwordCommand = "kpcli";
+  #      primary = true;
+  #      mbsync = {
+  #        enable = true;
+  #        expunge = "both";
+  #      };
+  #      realName = "Clayton Voges";
+  #    };
+  #  };
+  #};
+  programs = {
+    home-manager = {
+      enable = true;
+    };
+    git = {
+      enable = true;
+      userEmail = "vogesclayton@gmail.com";
+      userName = "cvoges12";
+    };
+    mbsync = {
+      enable = true;
+    };
+    firefox = {
+      enable = true;
+      enableAdobeFlash = false;
+    };
+    zsh = {
+      enable = true;
+      enableAutosuggestions = true;
+      enableCompletion = true;
+      #defaultKeymap = ;
+      history = {
+        expireDuplicatesFirst = true;
+        extended = true;
+        ignoreDups = true;
+        path = ".histfile";
+        save = 10000;
+        share = true;
+      };
+      initExtra = "";
+      profileExtra = "";
+      loginExtra = "";
+      logoutExtra = "";
+      #localVariables
+      #sessionVariables
+      #shellAliases
+      oh-my-zsh = {
+        enable = true;
+        #plugins
+        #theme
+      };
+      #plugins
+    };
+  };
+}
